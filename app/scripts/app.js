a40570f9922541b98cc663f71e78ecacef5b87d2
wang chao <cwang@thoughtworks.com>
2014-10-18 17:11:35 +0800

    using ajax

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -4,8 +4,7 @@ angular.module('routeApp', [
   'ngCookies',
   'ngSanitize',
   'ngRoute'
-])
-  .config(function ($routeProvider) {
+]).config(function ($routeProvider) {
     $routeProvider
       .when('/', {
         templateUrl: 'views/main.html',
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -5,17 +5,31 @@ angular.module('routeApp').controller('HomeController', function ($scope, $http)
     $scope.switchLight = function(){
         var request = {
             body: [
-                {
-                    "accessary_id": "abc",
-                    "switch": $scope.lightOn ? 1 : 0 }
+                {"accessory_id":"3-1","switch":"1"}
             ],
             uuid: "1234"
         };
         $scope.lightOn = !$scope.lightOn;
 
-//        $http.post("http://192.168.99.1:8090/smart", request, function(data){
+//        $http.post("http://192.168.99.1:8090/smart/setAccessory", request, function(data){
+//            console.log(data);
 //            $scope.lightOn = !$scope.lightOn;
 //        });
+//        data: {"body": [{"accessory_id":"3-1","switch":"1"}], "uuid": "1234"},
+
+
+        $.ajax({
+            type: "POST",
+            url:"http://192.168.99.1:8090/smart/setAccessory",
+            data: 'body=[{"accessory_id":"3-1","switch":"1"}]&uuid="1234"',
+            success: function(data){
+                alert(data);
+            }
+        });
+
+//        $http.jsonp("http://192.168.99.1:8090/smart/setAccessory?callback").success(data){
+//            console.log(data);
+//        }
     };
 
     $scope.color = {
