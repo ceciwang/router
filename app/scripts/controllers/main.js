a0149175a94bafccb86e9934aa7e4e636e75a1de
wang chao <cwang@thoughtworks.com>
2014-10-18 19:47:40 +0800

    able to controll multi lights

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,33 +1,42 @@
-'use strict';
+(function (_) {
+    'use strict';
 
-angular.module('routeApp').controller('HomeController', function ($scope, $http) {
-    $scope.lightOn = false;
-    $scope.switchLight = function(){
-        $scope.lightOn = !$scope.lightOn;
+    angular.module('routeApp').controller('HomeController', function ($scope, $http) {
+        $scope.lightOn = false;
 
-        var switchValue = $scope.lightOn ? 0 : 1;
+        var toRequestBody = function(id){
+            var switchValue = $scope.lightOn ? 0 : 1;
+            return '{"accessory_id":"' + id + '","switch":' + switchValue + '}';
+        };
 
-        var request = 'body=[{"accessory_id":"3-1","switch":'+ switchValue + '}]&uuid="1234"';
+        $scope.switchLight = function () {
+            $scope.lightOn = !$scope.lightOn;
 
-        $.ajax({
-            type: "POST",
-            url:"http://192.168.99.1:8090/smart/setAccessory",
-            data: request,
-            success: function(data){
+            var lightIds = ["accf23299c88202020202020", "3-1", "4F16-0D"];
+
+            var connected = _.map(lightIds, toRequestBody).join(",");
+            var request = 'body=[' + connected + ']&uuid="1234"';
+
+            $.ajax({
+                type: "POST",
+                url: "http://192.168.99.1:8090/smart/setAccessory",
+                data: request,
+                success: function (data) {
+                }
+            });
+        };
+
+        $scope.color = {
+            red: 255, green: 255, blue: 255
+        };
+
+        $scope.changeColor = function () {
+            var color = "rgb(" + $scope.color.red + "," + $scope.color.green + "," + $scope.color.blue + ")";
+            return {
+                "background-color": color
             }
-        });
-    };
 
-    $scope.color = {
-        red: 255, green: 255, blue: 255
-    };
-
-    $scope.changeColor = function() {
-        var color = "rgb(" + $scope.color.red + "," + $scope.color.green + "," + $scope.color.blue + ")";
-        return {
-            "background-color" : color
         }
 
-    }
-
-});
+    });
+})(window._);
