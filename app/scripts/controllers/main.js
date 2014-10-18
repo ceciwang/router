30a8695d6a0e3a30b67e8998d1d8971a2e322d07
wang chao <cwang@thoughtworks.com>
2014-10-18 20:38:05 +0800

    controll all lights

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,42 +1,54 @@
 (function (_) {
     'use strict';
 
-    angular.module('routeApp').controller('HomeController', function ($scope, $http) {
+    angular.module('routeApp').controller('HomeController', function ($scope) {
+
         $scope.lightOn = false;
 
-        var toRequestBody = function(id){
+        $scope.lights = [
+            {
+                id: "accf23299c88202020202020",
+                name: "客厅灯1",
+                selected: true,
+                lightOn: false
+            },            {
+                id: "3-1",
+                name: "客厅灯2",
+                selected: false,
+                lightOn: false
+            },           {
+                id: "4F16-0D",
+                name: "卧室灯",
+                selected: false,
+                lightOn: false
+            }
+        ];
+
+        var toRequestBody = function(light){
             var switchValue = $scope.lightOn ? 0 : 1;
-            return '{"accessory_id":"' + id + '","switch":' + switchValue + '}';
+            return '{"accessory_id":"' + light.id + '","switch":' + switchValue + '}';
         };
 
-        $scope.switchLight = function () {
-            $scope.lightOn = !$scope.lightOn;
+        $scope.switchLights = function () {
+            var body = _.chain($scope.lights)
+                .filter(function(light){return light.selected;})
+                .map(function(light){return toRequestBody(light)})
+                .value()
+                .join(",");
 
-            var lightIds = ["accf23299c88202020202020", "3-1", "4F16-0D"];
+            var request = 'body=[' + body + ']&uuid="1234"';
 
-            var connected = _.map(lightIds, toRequestBody).join(",");
-            var request = 'body=[' + connected + ']&uuid="1234"';
+            console.log(request);
 
             $.ajax({
                 type: "POST",
                 url: "http://192.168.99.1:8090/smart/setAccessory",
                 data: request,
                 success: function (data) {
+                    $scope.lightOn = !$scope.lightOn;
                 }
             });
         };
-
-        $scope.color = {
-            red: 255, green: 255, blue: 255
-        };
-
-        $scope.changeColor = function () {
-            var color = "rgb(" + $scope.color.red + "," + $scope.color.green + "," + $scope.color.blue + ")";
-            return {
-                "background-color": color
-            }
-
-        }
 
     });
 })(window._);
