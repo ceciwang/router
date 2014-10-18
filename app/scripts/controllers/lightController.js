1f1aba32d26530220f7cd30728987901347f1dbf
wang chao <cwang@thoughtworks.com>
2014-10-18 17:49:30 +0800

    change the rgb color

--- app/scripts/controllers/lightController.js
+++ app/scripts/controllers/lightController.js
@@ -10,7 +10,19 @@ angular.module('routeApp').controller('LightController', function ($scope) {
         return {
             "background-color" : color
         }
+    };
 
-    }
+    $scope.$watch('color', function(value, newVal){
+        var id = "4F16-0D";
+        var request = 'body=[{"accessory_id":"' + id + '","color":{"R":'+ $scope.color.red + ',"G":' + $scope.color.green + ',"B":' + $scope.color.blue + '}}]&uuid="1234"';
+
+        $.ajax({
+            type: "POST",
+            url:"http://192.168.99.1:8090/smart/setAccessory",
+            data: request,
+            success: function(data){
+            }
+        });
+    }, true)
 
 });
